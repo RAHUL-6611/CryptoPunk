1c3b4446d851818666aa4b67fe97d95984506804
rahul <rahulparmar7654321@gmail.com>
2022-01-19 20:00:11 +0530

    cors policy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 import Header from './components/Header';
-import CollectionCard from './components/CollectionCard';
+// import CollectionCard from './components/CollectionCard';
 import PunkList from './components/PunkList'
 import Main from './components/Main'
 
@@ -12,12 +12,14 @@ function App() {
   const [selectedPunk,setSelectedPunk] = useState(0)
 
   useEffect(()=>{
-    const getMyNfts = async()=>{
-      const nftData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x9b86606518753d11fc924159e4a51fe526911a5a&order_direction=asc')
+    const getMyNfts = async ()=>{
+      const nftData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x9b86606518753d11fc924159e4a51fe526911a5a&order_direction=asc',
+      {'Content-Type': 'text/plain'},
+      {"Access-Control-Allow-Origin": "*"})
       // console.log(nftData.data.assets); //https://testnets.opensea.io/assets/<asset_contract_address>/<token_id>
       setPunkListData(nftData.data.assets);
     }
-    console.log(punkListData);
+    // console.log(punkListData);
    getMyNfts();
   },[])
 
